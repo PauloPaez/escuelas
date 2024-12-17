4f1a96c85da09dd13b395fa604640c101b3ec330
Juanmorales1810 <juan1810.jm@gmail.com>
2024-12-17 12:34:38 -0300

    Agregar apiSlice para gestionar consultas y mutaciones de planillas con Redux Toolkit

---
+++ frontend/src/store/apiSlice.jsx
@@ -0,0 +1,25 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const objetosApi = createApi({
+    reducerPath: "objetosApi",
+    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8000/" }),
+    tagTypes: ["Planillas"], // Define tus tagTypes aquí
+    endpoints: (builder) => ({
+        getPlanillasMensuales: builder.query({
+            query: ({ mes, anio, escuela, direccion }) =>
+                `planillas/${mes}/${anio}/${escuela}/${direccion}`,
+            providesTags: ["Planillas"], // Asocia esta consulta con el tag 'Planillas'
+        }),
+        savePlanilla: builder.mutation({
+            query: (planilla) => ({
+                url: "/planillas",
+                method: "POST",
+                body: planilla,
+            }),
+            invalidatesTags: ["Planillas"],
+        }),
+    }),
+});
+
+export const { useGetPlanillasMensualesQuery, useSavePlanillaMutation } =
+    objetosApi;
